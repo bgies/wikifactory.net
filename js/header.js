983b8046e6cdf21a05c89366fc9086b89a61050f
mkampik <maximilian.kampik@gmail.com>
2015-03-10 23:15:37 +0000

    create animation for anchor tags.

--- js/header.js
+++ js/header.js
@@ -1,4 +1,4 @@
-// Hide Header on on scroll down
+// Hide Header on scroll down
 var didScroll;
 var lastScrollTop = 0;
 var delta = 10;
@@ -18,12 +18,11 @@ setInterval(function() {
 function hasScrolled() {
     var st = $(this).scrollTop();
 
-    // Make sure they scroll more than delta
+    // Make sure they scroll more than ∆
     if(Math.abs(lastScrollTop - st) <= delta)
         return;
 
-    // If they scrolled down and are past the navbar, add class .nav-up.
-    // This is necessary so you never see what is "behind" the navbar.
+    // If they scrolled down and are past the navbar, add class .nav-up
     if (st > lastScrollTop && st > navbarHeight){
         // Scroll Down
         $('header').removeClass('nav-down').addClass('nav-up');
