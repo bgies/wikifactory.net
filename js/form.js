1d163b54a10488185f7c594b3a7932cfe2d75092
mkampik <maximilian.kampik@gmail.com>
2015-03-31 12:24:28 +0100

    pass multiple classes into event functions.

--- js/form.js
+++ js/form.js
@@ -1,4 +1,4 @@
-// get early access submit
+// post email to datastore
 $('.submit').click(function(e){
     $.post( "/signup", {email:$('.access').val()}, function( data ) {
         $('.thankyou').addClass('visible');
@@ -6,7 +6,7 @@ $('.submit').click(function(e){
     e.preventDefault();
     e.stopPropagation();
 });
-// contact form submit
+// post contact form values to data store
 $('.tiny-submit').click(function(e){
     $.post( "/contact", {name:$('.contactname').val(), email:$('.contactemail').val(), subject:$('.contactsubject').val(), message:$('.contactmessage').val()}, function( data ) {
         $('.contactform').removeClass('visible');
@@ -14,39 +14,14 @@ $('.tiny-submit').click(function(e){
     e.preventDefault();
     e.stopPropagation();
 });
-// copen general contact form when clicking on the following buttons
-$('.large-btn').click(function(e){
+// open contact form when clicking on these buttons
+$('.large-btn, .small-btn, .contact, .new').click(function(e){
     e.preventDefault();
     e.stopPropagation();
     $('.contactform').addClass('visible');
 });
-$('.small-btn').click(function(e){
-    e.preventDefault();
-    e.stopPropagation();
-    $('.contactform').addClass('visible');
-});
-$('.contact').click(function(e){
-    e.preventDefault();
-    e.stopPropagation();
-    $('.contactform').addClass('visible');
-});
-$('.new').click(function(e){
-    e.preventDefault();
-    e.stopPropagation();
-    $('.contactform').addClass('visible');
-});
-// close general contact form when clicking on wrappers or cancel button
-$('.projects-wrapper').click(function(e){
-    e.preventDefault();
-    e.stopPropagation();
-    $('.contactform').removeClass('visible');
-});
-$('.network-nodes').click(function(e){
-    e.preventDefault();
-    e.stopPropagation();
-    $('.contactform').removeClass('visible');
-});
-$('.cancel').click(function(e){
+// close general contact form when clicking on these divs
+$('.projects-wrapper, .network-nodes, .cancel').click(function(e){
     e.preventDefault();
     e.stopPropagation();
     $('.contactform').removeClass('visible');
